60d85a30be013d248997d1538d2c0ffc8329c941
Thanaboon Sirirungnapawilai <thanaboon.sirirungnapawilai@gmail.com>
2021-02-09 17:38:39 +0700

    3

--- app.py
+++ app.py
@@ -3,4 +3,10 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
-app.mount('/')
+app.mount('/static', StaticFiles(directory='static'), name='static')
+templates = Jinja2Templates(directory='templates')
+
+
+@app.get('/')
+def index(request: Request):
+    return templates.TemplateResponse('index.html', context={'request': request})
